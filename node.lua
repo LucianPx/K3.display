8f9b6985138b36cc7fa14a7ceca19ac5222eef1c
LucianPx <Lucian@live.de>
2014-10-08 13:17:00 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -165,7 +165,7 @@ function node.render()
             end
             CONFIG.title_font:write(
                 10, HEIGHT - CONFIG.title_size - 5, 
-                CONFIG.playlist,
+                table.concat(CONFIG.playlist),
                 CONFIG.title_size,
                 1, 1, 1, alpha
             )
