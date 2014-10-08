59462252ca972b44b10e6e06d4954eb553e09635
LucianPx <Lucian@live.de>
2014-10-08 13:31:59 +0200

    Update iblib.lua

--- iblib.lua
+++ iblib.lua
@@ -42,7 +42,7 @@ function playlist(opt)
         end
 
         if state == "getnext" then
-            if string.find(opt.asset_name, "d2014c077") == nil then
+            if string.find(next_item.file, "d2014c077") == nil then
                 has_next, next_item = opt.get_next_item()
                 if not has_next then
                     print("no item")
